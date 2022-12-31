0d4671d484e129f7369f0667d0745d8181dcde30
TRudenko22 <timothy.rudenkom@gmail.com>
2022-12-31 17:02:50 -0500

    added output and formatting

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-    "os"
 	"net/smtp"
+	"os"
 
 	"github.com/spf13/viper"
 )
@@ -17,56 +17,66 @@ type Email struct {
 	Body       string
 }
 
-// handles all of the sending logic
+// Handles all of the sending logic
 func (e *Email) Send(server, port string) (err error) {
 
 	// Auth via Sender app password
-    auth := smtp.PlainAuth("", e.Sender, e.Pass, server,)
+	auth := smtp.PlainAuth("", e.Sender, e.Pass, server)
 
-    err = smtp.SendMail(
-	    fmt.Sprintf("%v:%v", server, port),
+	err = smtp.SendMail(
+		fmt.Sprintf("%v:%v", server, port),
 		auth,
 		e.Sender,
 		e.Recipients,
 		[]byte(fmt.Sprintf("Subject: %v \r\n\r\n%v", e.Subject, e.Body)),
-	) 
-  
-    return
+	)
+
+	for _, i := range e.Recipients {
+		log.Printf("%v  -->  %v\n", e.Sender, i)
+	}
+
+	return
 }
 
+func (e *Email) Output() {
+	fmt.Printf("Sender  : %v\n", e.Sender)
+	fmt.Printf("Subject : %v\n", e.Subject)
+}
+
+// Creates /home/$USER/.config/beacon directory
 func SetPathFile() (string, error) {
-    homeDir, err := os.UserHomeDir()
-    if err != nil {
-        log.Fatal("error finding home")
-        return "", err
-    }
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("error locating home directory")
+	}
 
-    path := fmt.Sprintf("%v/.config/beacon/", homeDir)
+	path := fmt.Sprintf("%v/.config/beacon/", homeDir)
 
-    if _, err = os.Stat(path); os.IsNotExist(err) {
-        err = os.MkdirAll(path, 0750)
-        if err != nil {
-            return "", err
-        }
-    } 
+	if _, err = os.Stat(path); os.IsNotExist(err) {
+		err = os.MkdirAll(path, 0750)
+		if err != nil {
+			return "", fmt.Errorf("error creating %v", path)
+		}
+	}
 
-    return path + "beacon.yml", nil
+	return path + "beacon.yml", nil
 }
 
+// Takes ~/.config/beacon/beacon.yml and returns an Email struct
 func LoadConfig() (config Email, err error) {
-    
-    pathFile, pathError := SetPathFile()
-    if pathError != nil {
-        return 
-    }
+
+	pathFile, pathError := SetPathFile()
+	if pathError != nil {
+		return
+	}
 
 	config_file := viper.New()
-    config_file.SetConfigType("yaml")
-    config_file.SetConfigFile(pathFile)
+	config_file.SetConfigType("yaml")
+	config_file.SetConfigFile(pathFile)
 
 	err = config_file.ReadInConfig()
 	if err != nil {
-        return 
+		return Email{}, fmt.Errorf("error reading in config file")
 	}
 
 	err = config_file.Unmarshal(&config)
@@ -78,11 +88,13 @@ func main() {
 
 	email, err := LoadConfig()
 	if err != nil {
-    	log.Fatal("Error reading configuration: ", err)
+		log.Fatal(err)
 	}
 
+	email.Output()
+
 	err = email.Send("smtp.gmail.com", "587")
-  	if err != nil {
-    	log.Fatal("Error Sending Email: Check Configuration Variables")
-  	}
+	if err != nil {
+		log.Fatal("Error Sending Email: Check Configuration Variables")
+	}
 }
